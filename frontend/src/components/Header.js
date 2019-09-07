099bca3ae4b5065c59ec449057745efef4266d7b
Tom Basche <tcbasche@gmail.com>
2019-09-07 13:16:44 +1000

    Fix up the header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-import background from '../img/canberra.jpeg';
 import './Header.css';
 
 export default class Header extends Component {
 
   render() {
     return (
-      <header className="App-header" style={{background: `url(${background}) no-repeat`, backgroundSize: 'cover'}}>
+      <header className="App-header">
         <div className="header-container">
             <div className="App-title">Slider mania</div>
         </div>
